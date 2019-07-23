0faa204baad287f97f594955b31aa8d0380d8371
AnnaGigoyan <anna.gigoyan.y@tumo.org>
2019-07-23 18:58:49 +0400

    changed Socket.js

--- socket.js
+++ socket.js
@@ -17,7 +17,7 @@ function usersNamespace(io) {
 
       db.getClient().collection("students").findOneAndUpdate(
         {email: user.email},
-        {$set: {'loggedIN' : true}},
+        {$set: {'loggedIn' : true}},
         {returnOriginal: false},
         function(err, results){
           if(err) {
@@ -46,21 +46,19 @@ function usersNamespace(io) {
           }
         });
     });
-    // TODO: add listener for starting chat
-    
-    // TODO: add listener to chat message
-
-    // TODO: add listener for editor message WYSIWIG
-
-    // TODO: add listener for drawing
-
-    // TODO: add listener for logging in, update flag loggedIn in Database, join room
-
-    // TODO: add listener on 'disconnect' to log out user, and emit
-
-    // TODO: add listener for logout message, update db, emit
-    
-    // TODO: add listener to search query
+    socket.on('search', (query, fn) => {
+      const textQuery = { $text: { $search: query } };
+      const learningTargetsQuery = { learningTargets: query };
+      const criteria = query ? { $or: [textQuery, learningTargetsQuery] } : {};
+      db.getClient().collection('students').find(criteria).sort({}).toArray(function (err, result) {
+        if (err) {
+          socket.emit('list errors', err);
+        } else {
+          console.log(result);
+          fn(result);
+        }
+      });
+    });
   });
 }
 
